b75b8a9b44fb4dc591bfd2ddd918daad4dc51982
Rishi Boyat <rishi.boyat@gmail.com>
2025-10-26 17:19:07 +0800

    Add files via upload

---
+++ Slots_App/Slots_App 2/Slots_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_AppApp.swift
+//  Slots_App
+//
+//  Created by HKIS on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Slots_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
